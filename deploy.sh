2fae8cb0486f96b65f244c98e7c979cf2487eb41
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-07-24 11:46:41 +0530

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 git pull origin main
 npm install
 npm run build
-npm run start
+
+pm2 stop next-app || true
+pm2 start npm --name "next-app" --start
+pm2 save
+
+echo "Deployment completed! App is running with PM2."
