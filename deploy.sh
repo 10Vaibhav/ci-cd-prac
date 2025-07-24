ff831727170e5f2c129e9ff06364d5f42e61d0e7
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-07-24 12:33:03 +0530

    Fix the path issue in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
+# Put system paths first to use global PM2
+export PATH=/usr/local/bin:/usr/bin:/usr/sbin:/sbin:$PATH
+
+
 git pull origin main
 npm install
 npm run build
 
 pm2 stop next-app || true
-pm2 start npm --name "next-app" --start
+pm2 delete next-app || true
+pm2 start npm --name "next-app" -- start
 pm2 save
 
 echo "Deployment completed! App is running with PM2."
