d00bf6611e254f674c8ab0df8aa1bf76b76c6fb3
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-07-24 12:23:32 +0530

    fixing

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm install
 npm run build
 
 pm2 stop next-app || true
-pm2 start npm --name "next-app" -- start
+pm2 start npm --name "next-app" --start
 pm2 save
 
 echo "Deployment completed! App is running with PM2."
